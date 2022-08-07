448f040d10718f1024daa721a01f14da0d5a016a
nkoguchi <nkoguchi00dev@gmail.com>
2022-08-08 02:23:50 +0900

    WIP

--- api/app/main.py
+++ api/app/main.py
@@ -1,10 +1,26 @@
-from typing import Union
+from tokenize import String
+from typing import List
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class Item(BaseModel):
+    remote: str
+    time: str
+    method: str
+    path: str
+    code: int
+    size: int
+    req_time: float
+    agent: str
+    XFF: str
+    req_id: str
+
+
 @app.post("/")
-def read_root():
-    return {"Hello": "World"}
+def update_item(item: str = Body(...)):
+    print(item)
+    return item
